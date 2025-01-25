82d410dc85f60c575d5e1ac6b3d505bc86f8f3cb
Aman11b <amansingh1100b@gmail.com>
2025-01-25 15:04:33 +0530

    mapping ingredient list (next learn react state)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,40 @@
 export default function Main(){
+    const ingredients=['chicken','oregano','tomato']
+
+    const ingredientListItems=ingredients.map(ingredient=>(
+        <li key={ingredient}>
+            {ingredient}
+        </li>
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        const formData=new formData(event.currentTarget)
+        const newIngredient=formData.get('ingredient')
+
+        ingredients.push(newIngredient);
+
+    }
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form 
+                className="add-ingredient-form"
+                onSubmit={handleSubmit}
+            >
                 <input 
                     aria-label="add ingredient"
                     type="text"
                     placeholder="e.g. oregano"
+                    name="ingredient"
                 />
                 <button>
                     Add Ingredient
                 </button>
             </form>
+            <ul>
+                {ingredientListItems}
+            </ul>
         </main>
     )
 }
