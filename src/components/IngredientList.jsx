8d7ae3e0f2ce8fa56f03a9e05c293c45891174a6
Aman11b <amansingh1100b@gmail.com>
2025-02-01 22:57:45 +0530

    refracted ClaudeRecipe & IngredientList in seperate components

---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+export default function IngredientList(props){
+    
+    const ingredientListItems = props.ingredients.map(ingredient=>(
+        <li key={ingredient}>
+            {ingredient}
+        </li>
+    ))
+
+    return(
+        <section className="container_section">
+                <h2>Ingredients on hand</h2>
+                <ul
+                    className="ingredient-list"
+                    aria-live="polite"
+                >
+                    {ingredientListItems}
+                </ul>
+                {props.ingredients.length > 3 && <div
+                    className="get-recipe-container"
+                >
+                    <div className="button_div">
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from you list of ingredients.</p>
+                    </div>
+
+                    <button
+                        onClick={props.toggleRecipeShown}
+                    >Get a recipe</button>
+
+                </div>}
+            </section>
+    )
+}
